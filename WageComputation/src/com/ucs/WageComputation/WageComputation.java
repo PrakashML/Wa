c4057b982c7b968d66e5a75ee664702d22896091
PrakashML <prakashkrishnan08@gmail.com>
2024-01-24 12:40:21 +0530

    Feature Added: monthly wage

--- WageComputation/src/com/ucs/WageComputation/WageComputation.java
+++ WageComputation/src/com/ucs/WageComputation/WageComputation.java
@@ -4,13 +4,13 @@ public class WageComputation {
     public static final int is_part_time = 1;
     public static final int is_full_time = 2;
     public static final int emp_rate_per_hour = 20;
+    public static final int num_of_working_days = 20;
     public static void main(String[] args) {
 
-            int empHrs = 0;
-            int empWage = 0;
-
+        int empHrs = 0, empWage = 0, totalEmpWage = 0;
+        for (int day = 0; day < num_of_working_days; day++) {
             int empCheck = (int) Math.floor(Math.random() * 10) % 3;
-            switch (empCheck){
+            switch (empCheck) {
                 case is_part_time:
                     empHrs = 4;
                     break;
@@ -21,6 +21,9 @@ public class WageComputation {
                     empHrs = 0;
             }
             empWage = empHrs * emp_rate_per_hour;
-            System.out.println("Emp Wage: " + empWage);
+            totalEmpWage += empWage;
+
+        }
+        System.out.println("Total Emp Wage: " + totalEmpWage);
     }
 }
