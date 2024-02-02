06c7e5bdd9c51fd66bbe00a190f4e97b4563f1d3
PrakashML <prakashkrishnan08@gmail.com>
2024-02-02 22:05:19 +0530

    Multiple company details using api approach

---
+++ WageComputation/src/com/ucs/WageComputation/IComputeEmpWage.java
@@ -0,0 +1,6 @@
+package com.ucs.WageComputation;
+
+public interface IComputeEmpWage {
+    public void addCompanyEmpWage(String company, int emp_rate_per_hour, int num_of_working_days, int max_hrs_in_month);
+    public void computeEmpWage();
+}
--- WageComputation/src/com/ucs/WageComputation/WageComputation.java
+++ WageComputation/src/com/ucs/WageComputation/WageComputation.java
@@ -1,5 +1,5 @@
 package com.ucs.WageComputation;
-public class WageComputation {
+public class WageComputation implements IComputeEmpWage {
     public static final int is_part_time = 1;
     public static final int is_full_time = 2;
 
@@ -10,18 +10,18 @@ public class WageComputation {
         companyEmpWageArray = new CompanyEmpWage[5];
     }
 
-    private void addCompanyEmpWage(String company, int emp_rate_per_hour, int num_of_working_days, int max_hrs_in_month){
+    public void addCompanyEmpWage(String company, int emp_rate_per_hour, int num_of_working_days, int max_hrs_in_month){
         companyEmpWageArray[numOfCompany] = new CompanyEmpWage(company, emp_rate_per_hour, num_of_working_days, max_hrs_in_month);
         numOfCompany++;
     }
 
-    private void computeEmpWage(){
+    public void computeEmpWage(){
         for (int i = 0; i < numOfCompany; i++){
             companyEmpWageArray[i].setTotalEmpWage(this.computeEmpWage(companyEmpWageArray[i]));
             System.out.println(companyEmpWageArray[i]);
         }
     }
-    private int computeEmpWage(CompanyEmpWage companyEmpWage) {
+    public int computeEmpWage(CompanyEmpWage companyEmpWage) {
 
         int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
         while (totalEmpHrs <= companyEmpWage.max_hrs_in_month && totalWorkingDays < companyEmpWage.num_of_working_days) {
@@ -53,4 +53,3 @@ public class WageComputation {
     }
 
 }
-
